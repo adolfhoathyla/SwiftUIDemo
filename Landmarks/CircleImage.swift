4c5721fca837694ba3c1b79b58e350a023622855
Athyla Beserra <adolfho.beserra@invillia.com>
2020-05-31 17:02:02 -0300

    first commit

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,21 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Adolfho Athyla de Almeida Beserra on 31/05/20.
+//  Copyright © 2020 Adolfho Athyla de Almeida Beserra. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
